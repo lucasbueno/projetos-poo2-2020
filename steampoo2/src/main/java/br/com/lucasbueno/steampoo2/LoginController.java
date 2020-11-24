32481add7133bfcb49d12c4618442753c03ef4f2
lucasbueno <lucasbuenolb@gmail.com>
2020-11-24 12:25:22 -0300

    our first window with fxml

---
+++ steampoo2/src/main/java/br/com/lucasbueno/steampoo2/LoginController.java
@@ -0,0 +1,18 @@
+package br.com.lucasbueno.steampoo2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+	@FXML
+	private Button btnOi;
+	
+	@FXML
+	private void actionBtnOi() {
+		if(btnOi.getText().contentEquals("Oi"))
+			btnOi.setText("Tchau");
+		else
+			btnOi.setText("Oi");
+	}
+
+}

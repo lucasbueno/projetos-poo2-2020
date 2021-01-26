29013b2e07bede2c2d2e870ed23b6554c5928a5e
lucasbueno <lucasbuenolb@gmail.com>
2021-01-26 13:03:09 -0300

    filling user attributes

--- steampoo2/src/main/java/br/com/lucasbueno/steampoo2/Usuario.java
+++ steampoo2/src/main/java/br/com/lucasbueno/steampoo2/Usuario.java
@@ -1,5 +1,28 @@
 package br.com.lucasbueno.steampoo2;
 
 public class Usuario {
+	private String username;
+	private String password;
 
+	public Usuario(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }

0cd8b66fb7a158890854bfda176793a50d49ae9f
lucasbueno <lucasbuenolb@gmail.com>
2021-01-27 13:46:26 -0300

    consuming API

--- steampoo2/src/main/java/br/com/lucasbueno/steampoo2/App.java
+++ steampoo2/src/main/java/br/com/lucasbueno/steampoo2/App.java
@@ -1,6 +1,14 @@
 package br.com.lucasbueno.steampoo2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /**
@@ -12,10 +20,39 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stge) {
+
+		List<String> users = consultAPI();
+		consumeAPI(users);
+
 		stage = stge;
 		stage.setScene(FXMLUtil.loadScene("login"));
 		changeResizable();
 		stage.show();
+	}
+
+	private void consumeAPI(List<String> users) {
+		for (String user : users)
+			System.out.println(user);
+	}
+
+	private List<String> consultAPI() {
+		List<String> result = new ArrayList<>();
+		try {
+			URL url = new URL("http://www.lucasbueno.com.br/steam.json");
+			URLConnection uc = url.openConnection();
+			InputStreamReader input = new InputStreamReader(uc.getInputStream());
+			BufferedReader in = new BufferedReader(input);
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				result.add(inputLine);
+
+			in.close();
+		} catch (Exception e) {
+			Alert alert = ExceptionUtil.error("Erro", "Erro ao consumir a API!", "Erro ao consumir a API!", e);
+			alert.showAndWait();
+		}
+		return result;
 	}
 
 	public static void setRoot(String fxml) {
@@ -25,9 +62,9 @@ public class App extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	public static void changeResizable() {
-		if(stage.isResizable())
+		if (stage.isResizable())
 			stage.setResizable(false);
 		else
 			stage.setResizable(true);
